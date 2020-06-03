acdd67e2653adac85dd94c10e7ce23425d56e562
黄腾霄 <huangtengxiao@cvte.com>
2020-06-03 08:24:16 +0800

    初始化electron

--- app/main/index.js
+++ app/main/index.js
@@ -1,1 +1,19 @@
-const {app,BrowserWindow}
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+let win;
+app.on('ready', () => {
+    win = new BrowserWindow({
+        width: 600,
+        height: 300,
+        webPreference: {
+            nodeIntegration: true,
+        }
+    });
+    if (isDev) {
+        win.loadURL('http://localhost:3000');
+    }
+    else {
+        win.loadFile(path.resolve(__dirname, '../render/pages/main/index.html'));
+    }
+});
